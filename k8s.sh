1eb670c371a32fbe7dab40639f753ff6f99f7078
mogi240 <63759306+mogi240@users.noreply.github.com>
2020-05-06 19:55:19 +0900

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -4,3 +4,7 @@ sudo -i
 kubectl run mykube-nginx --image=mogi240/my-nginx --port=80
 kubectl scale deployment mykube-nginx --replicas=20
 kubectl get deployments
+-----------------------------
+[root@node1 ~]# kubectl get deployments
+NAME           READY   UP-TO-DATE   AVAILABLE   AGE
+mykube-nginx   20/20   20           20          21m
